14a58175ccfae0748c4394b010c9d868a89b1123
Boutheldja Kaouther <kaoutherboutheldja@gmail.com>
2024-02-20 09:24:14 -0800

    sorting algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,39 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order.
- * @list: the list to be sorted.
+ * insertion_sort_list - sort a doubly linked list of integers in ascending
+ * order using the insertion sort algorithm
+ * @list: doubly linked list of integers
+ * Return: void
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *tmp;
+	listint_t *j, *temp, *curr;
 
-	if (list == NULL || *list == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
 
-	current = (*list)->next;
-	while (current != NULL)
+	curr = (*list)->next;
+	while (curr)
 	{
-		tmp = current;
-		current = current->next;
-
-		while (tmp && tmp->prev && tmp->prev->n > tmp->n)
+		j = curr;
+		while (j->prev && j->n < j->prev->n)
 		{
-			tmp->prev->next = tmp->next;
-			if (tmp->next != NULL)
-				tmp->next->prev = tmp->prev;
+			temp = j->prev;
+			if (temp->prev)
+				temp->prev->next = j;
+			j->prev = temp->prev;
+			temp->prev = j;
+			temp->next = j->next;
+			if (j->next)
+				j->next->prev = temp;
+			j->next = temp;
 
-			tmp->next = tmp->prev;
-			tmp->prev = tmp->prev->prev;
-			tmp->next->prev = tmp;
-
-			if (tmp->prev != NULL)
-				tmp->prev->next = tmp;
-			else
-				*list = tmp;
+			if (!j->prev)
+				*list = j;
 			print_list(*list);
 		}
+		curr = curr->next;
 	}
 }
